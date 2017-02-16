f0e835b7912aac49152d1deed9f4a342f91d682a
Nate <sauceynate@gmail.com>
2017-02-16 16:06:32 -0600

    button css, base constructor for the hero made, stat randomizer made

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 console.log('im working')
+
+
+
+//functions being used in the constructors
+function statNumbers(min, max) {
+  min = Math.ceil(5);
+  max = Math.floor(30);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//naming input
+var nameMe = prompt('give me a name');
+function TheHero (name, hitPoints, mana, str, wis){
+	this.name = name || '';
+	this.hitPoints = hitPoints || '';
+	this.mana = mana || '';
+	this.str = str || '';
+	this.wis = wis || '';
+}
+
+var newHero = new TheHero( nameMe, statNumbers(), statNumbers(), statNumbers(), statNumbers());
